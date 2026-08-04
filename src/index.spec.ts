b94db5b48f0c37f32599759f0e67b30d884ea7f0
Calvin <calvinverso@gmail.com>
2026-08-04 14:28:59 -0600

    Failing on multiple numbers separated by comma

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -15,7 +15,19 @@ describe("when summing one number", () => {
 })
 
 // 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
+describe("when inputting two numbers", () => {
+  it('should return the sum of those two numbers', () => {
+    expect(calc('1,2')).toEqual(3);
+  })
+})
+
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+describe("when inputting unknown amount of numbers", () => {
+  it('should return the sum of all those numbers', () => {
+    expect(calc("1,2,3")).toEqual(6);
+  })
+})
+
 // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
 // 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
 // 7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export function calc(_input: string): number {
-  return 0;
+  if(!_input.includes(","))
+    return Number(_input);
+  
+  let numbers = _input.split(",");
+  return Number(numbers[0]) + Number(numbers[1]);
 }
