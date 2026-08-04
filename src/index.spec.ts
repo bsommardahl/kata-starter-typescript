dc6004252f122b1847634a406e2efbac456e45ef
Byron Sommardahl <byron@sommardahl.com>
2026-08-04 20:50:30 +0000

    Failing on negative number

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -43,6 +43,12 @@ describe("when passing in a custom delimeter", () => {
 })
 
 // 7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
+describe("when summing with a negative number", () => {
+  it("should throw an exception with the offending number", () => {
+    expect(() => calc("1,-2,3")).toThrow("Negatives not allowed: -2")
+  })
+})
+
 // 8. Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers).
 // 9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
 // 10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1**_2_**3” should return 6)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export function calc(_input: string): number {
-  if(!_input.includes(","))
-    return Number(_input);
+  let delim = ",";
+  if(_input.startsWith("//")){
+    const parts = _input.split("\n");
+    delim = parts[0].replace("//", "");
+    _input = parts[1];
+  }
 
-  let numbers = _input.replace("\n", ",").split(",");
+  let numbers = _input.replace("\n", delim).split(delim);
   return numbers.map(x=> Number(x)).reduce((sum, next) => sum + next, 0);
 }
