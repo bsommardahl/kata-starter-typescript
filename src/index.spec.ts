7d2a1eafcf25191c107e18f616366f710d1afe86
Byron Sommardahl <byron@sommardahl.com>
2026-09-01 19:19:39 +0000

    Failing on the new line delim

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -28,6 +28,12 @@ describe("when calculating any amount of number", () => {
   });
 });
 // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
+describe("when calculating with a new line delimiter", () => {
+  it("should calculate all the numbers", () => {
+    expect(calc("1\n2,3")).toEqual(6);
+  })
+})
+
 // 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
 // 7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
 // 8. Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers).
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export function calc(_input: string): number {
   if (!_input) return 0;
   if (_input.length === 1) return parseInt(_input);
-  const inputArr = _input.split(",");
-  return parseInt(inputArr[0]) + parseInt(inputArr[1]);
+  const inputArr = _input.split(",").map(x=> parseInt(x));
+  return inputArr.reduce((a,b) => a+b,0);
 }
